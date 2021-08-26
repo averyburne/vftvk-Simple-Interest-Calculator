07fa67a4efc9fbc3f9f3aa67bd0f2c39c401dbb5
Avery <burneav@gmail.com>
2021-08-26 13:26:56 +0000

    Added JS functionality for calculations

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+let principal = document.getElementById('principal').value;
+let rate = document.getElementById('rate').value;
+let years = document.getElementById('years').value;
+let interest = principal * years * rate / 100;
+let year = new Date().getFullYear() + parseInt(years);
+
+function updateRate() {
+    let rateval = document.getElementById('rate').value;
+    document.getElementById("rate_val").innerText = rateval
+}
+
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    document.getElementById('result').innerHTML = `If you deposit ${principal},\<br\>at an interest rate of ${rate}%\<br\>You will receive an amount of ${interest},\<br\>in the year ${year}\<br\>`
 }
         
